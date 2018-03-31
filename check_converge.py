19f28460d26314990e62cfbbb1869b1731594f31
carstencww <carstencww@gmail.com>
2018-04-01 00:58:07 +0800

    finish check converge

---
+++ check_converge.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import numpy as np
+
+origin_centroid = [0]*10
+
+with open("./train_centroid.txt","r") as cens:
+	for cen in cens:
+		cen = cen.strip()
+		class_no , centroid = cen.split('\t')
+		class_no = int(class_no)
+		centroid = centroid.split(",")
+		origin_centroid[class_no] = [float(x) for x in centroid]
+origin_centroid = np.asarray(origin_centroid)
+
+
+result_centroid = [0]*10
+
+with open("./centroid_result.txt","r") as cens:
+	for cen in cens:
+		cen = cen.strip()
+		class_no , centroid = cen.split('\t')
+		class_no = int(class_no)
+		centroid = centroid.split(",")
+		result_centroid[class_no] = [float(x) for x in centroid]
+result_centroid = np.asarray(result_centroid)
+diff = np.linalg.norm(origin_centroid - result_centroid, axis=1)
+dist = diff.sum()
+if dist<100:
+	print("0")
+else: 
+	print("1")
+print(dist)
--- part1a_run.sh
+++ part1a_run.sh
@@ -2,7 +2,7 @@
 
 OUT_FILE=/user/ChanCarsten/hw1a
 IN_FILE=/user/ChanCarsten/input/image_train.txt
-echo $PWD
+kmean () {
 hadoop dfs -rm -R $OUT_FILE
 hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar \
 -D mapred.map.tasks=2 \
@@ -12,6 +12,25 @@ hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar \
 -file $PWD/part1a_red.py -reducer part1a_red.py \
 -input $IN_FILE \
 -output $OUT_FILE
-
 hadoop fs -getmerge $OUT_FILE ./centroid_result.txt
-hadoop dfs -rm -R $OUT_FILE
+}
+echo $PWD
+kmean
+converging=( $(./check_converge.py) )
+while [ ${converging[0]} = 1 ]; do
+mv centroid_result.txt train_centroid.txt
+echo ${converging[1]} >> log.txt
+kmean
+converging=( $(./check_converge.py) )
+done
+
+
+
+
+
+
+
+
+
+
+
