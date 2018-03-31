9d3fa8c03e0b5fb45a2dd6f07907db32be67e922
carstencww <carstencww@gmail.com>
2018-03-31 22:29:32 +0800

    finish runscript

--- Part1b_run1.sh
+++ Part1b_run1.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-OUT_FILE=/user/ChanCarsten/hw1b
+OUT_FILE=/user/ChanCarsten/hw1a
 IN_FILE=/user/ChanCarsten/input/*
 echo $PWD
 hadoop dfs -rm -R $OUT_FILE
 hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar \
--D mapred.map.tasks=24 \
--D mapred.reduce.tasks=12 \
--file $PWD/part1b_map1.py -mapper part1b_map1.py \
--file $PWD/part1b_red1.py -reducer part1b_red1.py \
+-D mapred.map.tasks=100 \
+-D mapred.reduce.tasks=10 \
+-file $PWD/part1b_map1.py -mapper part1a_map.py \
+-file $PWD/part1b_red1.py -reducer part1a_red.py \
 -input $IN_FILE \
 -output $OUT_FILE
 
-hadoop fs -getmerge $OUT_FILE ./supp.txt
+hadoop fs -getmerge $OUT_FILE ./centroid_result.txt
 hadoop dfs -rm -R $OUT_FILE
-
-hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar \
--D mapred.map.tasks=24 \
--D mapred.reduce.tasks=12 \
--file $PWD/supp.txt \
--file $PWD/part1b_map2.py -mapper part1b_map2.py \
--file $PWD/part1b_red2.py -reducer part1b_red2.py \
--input $IN_FILE \
--output $OUT_FILE
-
-hadoop fs -getmerge $OUT_FILE ./final.txt
-cat final.txt | sort -t$'\t' -k2nr | head -n40 > final_output.txt
