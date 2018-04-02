f378efc7df912aa6bc496b156517c13a640e0050
carstencww <carstencww@gmail.com>
2018-04-02 13:05:44 +0800

    upload result

--- read_label.py
+++ read_label.py
@@ -1,6 +1,7 @@
+#/usr/bin/env python
 import numpy as np
 np.random.seed(0)
-imagefile = "label_train.txt"
+imagefile = "./data/label_train.txt"
 cans = (2732, 35349, 33522, 22900, 16676, 21029, 32329, 26255, 13650, 56289)
 labels= []
 
